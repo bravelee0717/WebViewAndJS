6085c2b94a7072b91790756235179bab915e8c43
bravelee <171916592@qq.com>
2019-09-11 16:24:39 +0800

    commit WebViewAndJS

--- app/src/main/java/com/example/webviewandjs/activity/MainActivity.java
+++ app/src/main/java/com/example/webviewandjs/activity/MainActivity.java
@@ -36,6 +36,7 @@ public class MainActivity extends Activity {
             @Override
             public void onPageStarted(WebView view, String url, Bitmap favicon) {
                 Log.e("webview", "onPageStarted");
+                Log.e("webview", "onPageStarted");
             }
 
             @Override
